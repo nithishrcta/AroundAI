60ec6d9dbbf8a00bcc4033236c9a87f9972c41cc
R Nithish <nithishr81@gmail.com>
2023-04-24 18:38:44 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 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,
 });
 
 const openai = new OpenAIApi(configuration);
